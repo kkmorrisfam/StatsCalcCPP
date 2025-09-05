e4ba656d583fc73d3255d3a42c70b9c4c0360016
Kerri Morris <kkmorrisfam@hotmail.com>
2025-09-04 20:32:53 -0700

    Add input fields for files, add test files to project

--- src/render.cpp
+++ src/render.cpp
@@ -6,6 +6,13 @@
 
 #include "render.hpp"
 
+
+/**
+ * draw window
+ * call any class functions that are drawn in the window
+ *
+ *
+ */
 void WindowClass::Draw(std::string_view label)
 {
     constexpr static auto window_flags =
@@ -19,8 +26,55 @@ void WindowClass::Draw(std::string_view label)
 
     ImGui::Begin(label.data(), nullptr, window_flags);
 
-    ImGui::End();
+    //draw other boxes/buttons
+    DrawInputTextFields();
+    
+    ImGui::End();  //must close what is opened
 }
+
+/**
+ *1. need to have a two text boxes to import csv files;
+    collect month/year for stats
+ * or one which will accept multiple csv files
+ *2. read in csv file
+    store contents into data structure
+    variables: month & year
+    read by row matters file, store in map? add to vector? filter?
+    read by row events file, store in map? add to vector? filter?
+ *3. write results to text file
+    if file is not there, creates it, if it is there, writes over the old one
+    write header with current date/time; month date of stats
+
+ *4.Get list of opened cases within month/year input
+    Get subtotal by charge
+ *5.Get list of closed cases withing month/year
+    Get subtotal of cases by charge
+    Get subtotal by charge and by plea
+*6. Get subtotal of hours by charge closed
+*7. Helper: filter matters by APCON in map?
+*8. Helper: filter events by APCON
+*9. Helper: get get closed matters and events
+*10.Helper: get opened cases
+*11.Helper: get charge count
+*12.Helper: get dispostion count of closed cases
+*13.Helper: get subtotal hours by charges of closed cases
+ */
+void WindowClass::SaveToCsvFile(std::string_view filename)
+{
+
+}
+void WindowClass::LoadFromCsvFile(std::string_view filename)
+{
+
+}
+
+void WindowClass::DrawInputTextFields()
+{
+    ImGui::InputText("Matter File Name: ", matterFileNameBuffer, sizeof(matterFileNameBuffer));
+
+    ImGui::InputText("Events File Name: ", eventFileNameBuffer, sizeof(eventFileNameBuffer));
+}
+
 
 void render(WindowClass &window_obj)
 {
--- src/render.hpp
+++ src/render.hpp
@@ -7,6 +7,15 @@ class WindowClass
 {
 public:
     void Draw(std::string_view label);
+
+private:
+    void SaveToCsvFile(std::string_view filename);
+    void LoadFromCsvFile(std::string_view filename);
+    void DrawInputTextFields();
+
+private:
+    char matterFileNameBuffer[65]{};  //InputText expects char array
+    char eventFileNameBuffer[65]{};
 };
 
 void render(WindowClass &window_obj);
